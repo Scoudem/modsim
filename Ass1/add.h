2f27509a60c162944fe78d19b571e8abd903cdcb
Sjoerd Wenker <sjoerd.polo@gmail.com>
2015-10-26 12:48:24 +0100

    added header file for add.c and implemented Kahan summation for 1c

---
+++ Ass1/add.h
@@ -0,0 +1,17 @@
+float
+addRecursive(int from, int to);
+
+float
+addForward(int from, int to);
+
+float
+addReverse(int from, int to);
+
+double
+addRecursiveDouble(int from, int to);
+
+double
+addForwardDouble(int from, int to);
+
+double
+addReverseDouble(int from, int to);

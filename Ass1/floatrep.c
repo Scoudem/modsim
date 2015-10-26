050da0892bb3008beeb4a4364e17cd686533918d
Scoudem <tvvaalen@gmail.com>
2015-10-26 12:08:49 +0100

    function split

--- Ass1/floatrep.c
+++ Ass1/floatrep.c
@@ -8,11 +8,17 @@
 #include <float.h>
 #include <stdio.h>
 
-int
-main(int argc, char* argv[])
+void
+float_lookup()
 {
     fprintf(stdout, "==========Ranges defined by float.h==========\n");
     fprintf(stdout, "Float range:    %g - %g\n", FLT_MIN, FLT_MAX);
     fprintf(stdout, "Double range:   %g - %g\n", DBL_MIN, DBL_MAX);
     fprintf(stdout, "LDouble range:  %Lg - %Lg\n", LDBL_MIN, LDBL_MAX);
+}
+
+int
+main(int argc, char* argv[])
+{
+    float_lookup();
 }

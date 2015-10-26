e53602a345893b33064c04def70cd83bff2e0e35
Scoudem <tvvaalen@gmail.com>
2015-10-26 12:07:02 +0100

    float.h lookups

--- Ass1/floatrep.c
+++ Ass1/floatrep.c
@@ -5,8 +5,14 @@
  * Comments: 
  */
 
+#include <float.h>
+#include <stdio.h>
+
 int
 main(int argc, char* argv[])
 {
-    
+    fprintf(stdout, "==========Ranges defined by float.h==========\n");
+    fprintf(stdout, "Float range:    %g - %g\n", FLT_MIN, FLT_MAX);
+    fprintf(stdout, "Double range:   %g - %g\n", DBL_MIN, DBL_MAX);
+    fprintf(stdout, "LDouble range:  %Lg - %Lg\n", LDBL_MIN, LDBL_MAX);
 }

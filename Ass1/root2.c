030043ec1011e96192a720689f2bc2cd438cb888
Scoudem <tvvaalen@gmail.com>
2015-10-30 12:56:07 +0100

    3 done, 4 partial

--- Ass1/root2.c
+++ Ass1/root2.c
@@ -5,8 +5,62 @@
  * Comments: 
  */
 
-int
-main(int argc, char* argv[])
-{
+#include <stdio.h>
+#include <math.h>
+#define MAX_CYCLES 1000
+#define TOLERANCE pow(10, -7)
+#define CLOSE(a, b) (fabs(a - b) < TOLERANCE)
+#define sign(x) ((x > 0) - (x < 0))
 
+/*
+ * Binary search using to find root of function f given
+ * lower bound a, upper bound b.
+ * stores amount of steps and the root in given *steps, *root.
+ * returns whether it found root.
+ */
+int 
+binsearch_root(double a, double b, double (*f)(double), int *steps, double *root)
+{   
+    double c;
+
+    *steps = 0;
+    while(++*steps < MAX_CYCLES)
+    {   
+        c = (a + b) / 2;
+        *root = f(c);
+
+        if(CLOSE(*root, 0) || (b - a) / 2 < TOLERANCE)
+        {   
+            return 1;
+        }
+
+        if(sign(c) == sign(a)) {
+            a = c;
+        }  else {
+            b = c;
+        }
+    }
+
+    return 0;
+}
+
+double
+f(double x)
+{
+    return pow(x, 2) - 2;
+}
+
+int
+main()
+{
+    printf("======Finding roots using binary search======\n");
+    int steps, lower = 0, upper = 4;
+    double root;
+
+    int result = binsearch_root(lower, upper, &f, &steps, &root);
+    if(!result) {
+        printf("No root found after reaching MAX_CYCLES (%d)\n", MAX_CYCLES);
+    } else {
+        printf("Found root (%g) in %d steps (lower: %d, upper: %d)\n", root, steps, lower, upper);
+    }
 }

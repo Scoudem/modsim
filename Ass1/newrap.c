0a611ad4b0963ee71b8754edc2de9ee5a65b0c43
Scoudem <tvvaalen@gmail.com>
2015-11-06 11:58:53 +0100

    Find two roots at most

--- Ass1/newrap.c
+++ Ass1/newrap.c
@@ -7,23 +7,32 @@
 
 #include <stdio.h>
 #include <math.h>
-#define MAX_CYCLES 1000
+#define MAX_CYCLES 10000
 #define TOLERANCE pow(10, -7)
+#define EPSILON pow(10, -14)
 
 /**
  * Newton-Raphson method find root of function f given
  * derivative of f f_prime.
  */
 void 
-binsearch_root(float (*f)(float), float (*f_prime)(float))
+newrap_root(float (*f)(float), float (*f_prime)(float), float x0, char loop)
 {
     int steps = 0;
-    float x0 = 1, x1, y, y_prime;
+    float x1, y, y_prime;
+
+    printf("Starting from %g:\n", x0);
 
     while(++steps < MAX_CYCLES)
     {
         y = f(x0);
         y_prime = f_prime(x0);
+
+        if(fabs(y_prime) < EPSILON)
+        {
+            break;
+        }
+
         x1 = x0 - y / y_prime;
 
         if(fabs(x1 - x0) / fabs(x1) < TOLERANCE)
@@ -34,24 +43,33 @@ binsearch_root(float (*f)(float), float (*f_prime)(float))
         x0 = x1;
     }
 
-    printf("After %d steps: found root %g (x0^2=%g)\n", steps, x0, pow(x0, 2));
+    if(!isnan(x1))
+    {
+        printf("  After %d steps: found root %g\n", steps, x0);
+        if(loop) newrap_root(f, f_prime, -x0, 0);
+    }
+    else
+    {
+        printf("After %d steps: found no root\n", steps);
+        if(loop) newrap_root(f, f_prime, -x0, 0);
+    }
 }
 
 float
 f(float x)
 {
-    return pow(x, 2) - 2;
+    return pow(x, 3) + 3 * pow(x, 2) -4 ;
 }
 
 float
 f_prime(float x)
 {
-    return 2 * x;
+    return 3 * pow(x, 2) + 6 * x;
 }
 
 int
 main()
 {
     printf("=====Finding roots using Newton-Raphson======\n");
-    binsearch_root(&f, &f_prime);    
+    newrap_root(&f, &f_prime, 2, 1);    
 }

fdf7beba35b797d45bf3e2ec6aa755ebc680de74
Sjoerd Wenker <sjoerd.polo@gmail.com>
2015-11-26 23:12:25 +0100

    lotke volterra model

--- Ass2/lotka-volterra.py
+++ Ass2/lotka-volterra.py
@@ -8,32 +8,57 @@ Contains a Lotka-Volterra Model which is simulated using the RK4 method.
 '''
 from integration import RungeKutta4
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
-startvalues = [[11.0, 49.0], [1.0, 20.0]]
-#startvalues = [[1, 20]]
-objects = []
+'''
+Method that simulates a Lotka-Volterra Model using RK4 for the given
+functions with a list containing sets of startvalues and a list of timesteps
+This will show a window for each of the timesteps
+'''
+def lotke_volterra(functions, startvalues, dts, generations):
+    for dt in dts:
+        objects = []
+        for startvalue in startvalues:
+            objects.append(RungeKutta4(functions, 0, startvalue, stepsize=dt))
 
-for i,startvalue in enumerate(startvalues):
-    objects.append(RungeKutta4([
+        for m in objects:
+            m.generate_n(generations)
+
+        fig = plt.gcf()
+        fig.suptitle('Lotka-Volterra with dt:{} ({} timesteps)'.format(dt, generations), fontsize="x-large")
+        olen = len(objects)
+
+        ''' Make plots for each set of startvalues '''
+        for i, m in enumerate(objects):
+            ax = plt.subplot(2, olen, i + 1)
+            x = startvalues[i][0]
+            y = startvalues[i][1]
+            ax.set_title("Startvalues: x:{} y:{}".format(x, y))
+
+            ''' Plot values '''
+            values = m.get_y_values()
+            ax.plot(m.get_t_values(), values[0, :], label="x")
+            ax.plot(m.get_t_values(), values[1, :], label="y")
+            ax.legend(loc='best')
+
+            ''' Make scatter plot '''
+            ax = plt.subplot(2, olen, olen + i + 1)
+            ax.scatter(values[0, :], values[1, :])
+        plt.show()
+        plt.close()
+
+
+''' Main execution '''
+if __name__ == '__main__':
+    startvalues = [[11, 49], [1, 10], [15, 26]]
+    dt = [1, 0.1, 0.05, 0.01, 0.005, 0.001]
+    generations = 10000
+    functions = [
         lambda (t, x, y): -0.5 * x + 0.01 * x * y,
         lambda (t, x, y): y - 0.1 * x * y
-    ], 0, startvalue, stepsize=0.1))
+    ]
+    lotke_volterra(functions, startvalues, dt, generations)
 
-for i, result in enumerate(objects):
-    result.generate_n(1200)
-
-fig = plt.gcf()
-fig.suptitle('Lotka-Volterra', fontsize="x-large")
-
-for i, result in enumerate(objects):
-    plt.subplot(len(objects), 1, i + 1)
-    values = result.get_y_values()
-    x, y = values.shape
-    print values
-    for j in range(x):
-        plt.plot(result.get_t_values(), values[j, :])
-plt.show()
-plt.close()
 
 '''
 Calculations for stable point:

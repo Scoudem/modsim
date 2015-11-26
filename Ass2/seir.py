cd91895575125b11018e0f6f3d4f70fc39a0732d
Scoudem <tvvaalen@gmail.com>
2015-11-26 22:47:52 +0100

    Almost working version of seir

--- Ass2/seir.py
+++ Ass2/seir.py
@@ -8,22 +8,27 @@ SEIR model
 '''
 
 import integration as ig
-
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    N = 1000.0
-    t = 50.0 / N
-    eps = 1.0 / 6.0
-    mu = 1.0 / 9.0
-    beta = 0.04
-    b = 0.0
+    n = 1000.0  # population
+    beta = 0.96  # infection rate
+    gamma = 1.0 / 9.0  # recovery rate
+    sigma = 1.0 / 6.0  # conversion rate
+    mu = 0.0  # death rate (0 for stable pop)
+    nu = 0.0  # pass to resistant
 
-    st = lambda (t, s, e, i, r): N - beta * s * i - mu * s
-    se = lambda (t, s, e, i, r): beta * s * i - (eps + mu) * e
-    si = lambda (t, s, e, i, r): eps * e - (t + mu) * i
-    sr = lambda (t, s, e, i, r): t * i - mu * r
+    st = lambda (t, s, e, i, r): mu * (n - s) - beta * ((s * i) / n) - nu * s
+    et = lambda (t, s, e, i, r): beta * ((s * i) / n) - (mu + sigma) * e
+    it = lambda (t, s, e, i, r): sigma * e + (mu + gamma) * i
+    rt = lambda (t, s, e, i, r): gamma * i - mu * r + nu * s
 
-    m = ig.RungeKutta4([st, se, si, sr], 0, [N - 1, 1, 0, 0])
-    m.generate_n(10000)
-    print m
-    ig.plot(m)
+    m = ig.RungeKutta4([st, et, it, rt], 0, [999, 1, 0, 0], stepsize=0.01)
+    m.generate_n(100)
+
+    plt.plot(m.get_y_values()[0], label="S")
+    plt.plot(m.get_y_values()[1], label="E")
+    plt.plot(m.get_y_values()[2], label="I")
+    plt.plot(m.get_y_values()[3], label="R")
+    plt.legend(loc='best')
+    plt.show()

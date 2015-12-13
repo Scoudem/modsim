e20cd5e6f0aec28db335d651e6dfb08a9efad42b
Scoudem <tvvaalen@gmail.com>
2015-12-13 20:18:05 +0100

    default sim

--- Ass3/simulate.py
+++ Ass3/simulate.py
@@ -9,20 +9,14 @@ import nbody
 
 if __name__ == '__main__':
     model = nbody.model.Model()
-    # model.add_particle(10 ** 12, [100, 100], [-0.1, 0])
-    # model.add_particle(10 ** 13, [0, 0], [0.1, 0])
-    # model.add_particle(10 ** 10, [-50, 0], [0, 3])
-    # model.add_particle(10 ** 10, [50, 0], [0, -3])
-    # model.add_particle(10 ** 10, [0, -50], [3, 0])
-    # model.add_particle(10 ** 10, [0, 50], [-3, 0])
-    # # model.add_particle(10 ** 9, [25, 25], [0.02, 0.01])
-    # # model.add_particle(10 ** 5, [40, 10], [0.05, 0])
-    # # model.add_particle(10 ** 2, [-30, -50], [0.05, 0.03])
+    model.add_particle(10 ** 12, [0, 0], [0.2, 0])
+    model.add_particle(10 ** 7, [10, 2], [-0.5, 3])
+    # model.add_particle(10 ** 8, [20, 7], [1, 0.1])
 
     model.set_dt(0.1)
     model.set_size((-50, 50))
 
-    model.generate_random(3)
+    # model.generate_random(3)
 
-    model.next_n_timesteps(100)
+    model.next_n_timesteps(1000)
     model.plot(drawpath=True, autoscale=False)
